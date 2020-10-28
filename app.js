c0803655cec6721c23d0b549a7780b9659905a23
Devesh Kumar Singh <devdeveshsingh@gmail.com>
2020-10-28 22:01:24 +0530

    Use EJS to display weekday or weekend for current date

--- app.js
+++ app.js
@@ -3,9 +3,23 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get("/", (req, res) =>
 {
-    res.send("Hello");
+    const today = new Date();
+    const currentDay = today.getDay();
+    let day = "";
+
+    if (currentDay === 6 || currentDay === 0)
+    {
+        day = "weekend";
+    }
+    else
+    {
+        day = "weekday";
+    }
+    res.render('list', {dayType : day});
 })
 
 app.listen(3000, () =>
