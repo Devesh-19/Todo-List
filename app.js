e8db94f11d791e444cc67a41c5c46506943006ad
Devesh Kumar Singh <devdeveshsingh@gmail.com>
2020-10-28 22:15:32 +0530

    Display current day on browser

--- app.js
+++ app.js
@@ -11,14 +11,40 @@ app.get("/", (req, res) =>
     const currentDay = today.getDay();
     let day = "";
 
-    if (currentDay === 6 || currentDay === 0)
-    {
-        day = "weekend";
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+    
+        case 1:
+            day = "Monday";
+            break;
+    
+        case 2:
+            day = "Tuesday";
+            break;
+    
+        case 3:
+            day = "Wednesday";
+            break;
+    
+        case 4:
+            day = "Thursday";
+            break;
+    
+        case 5:
+            day = "Friday";
+            break;
+    
+        case 6:
+            day = "Saturday";
+            break;
+    
+        default:
+            console.log(`Error: Current day is equal to ${currentDay}`);
+            break;
     }
-    else
-    {
-        day = "weekday";
-    }
+
     res.render('list', {dayType : day});
 })
 
