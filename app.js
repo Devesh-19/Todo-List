fae9f8d08799bbb02e87b61eeab0c610572b9ee1
Devesh Kumar Singh <devdeveshsingh@gmail.com>
2020-10-29 01:49:12 +0530

    Display current date via local server

--- app.js
+++ app.js
@@ -8,42 +8,15 @@ app.set('view engine', 'ejs');
 app.get("/", (req, res) =>
 {
     const today = new Date();
-    const currentDay = today.getDay();
-    let day = "";
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-    
-        case 1:
-            day = "Monday";
-            break;
-    
-        case 2:
-            day = "Tuesday";
-            break;
-    
-        case 3:
-            day = "Wednesday";
-            break;
-    
-        case 4:
-            day = "Thursday";
-            break;
-    
-        case 5:
-            day = "Friday";
-            break;
-    
-        case 6:
-            day = "Saturday";
-            break;
-    
-        default:
-            console.log(`Error: Current day is equal to ${currentDay}`);
-            break;
-    }
+    const options = {
+        weekday: 'long',
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric'
+    };
+
+    const day = today.toLocaleDateString("en-IN", options);
 
     res.render('list', {dayType : day});
 })
